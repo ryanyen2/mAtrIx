432e24d95594270336493ae8b7c8a71a4a432c64
ryan-waterloo <r4yen@uwaterloo.ca>
2023-03-27 12:29:30 -0400

    recoil, bandits utils

--- src/App.js
+++ src/App.js
@@ -1,55 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-// import logo from "./logo.svg";
 import "./App.css";
-import Home from "./view/home";
-// bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "./components/AppBar";
+import {
+  BrowserRouter,
+  Link,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
+import Home from "./view/home";
+import Sample from "./view/sample";
 
-// create state
+
 
 function App() {
-  // use state to store data
-  const [data, setData] = useState([12, 5, 6, 6, 9, 10]);
-  const [width, setWidth] = useState(700);
-  const [height, setHeight] = useState(500);
-  const [id, setId] = useState("root");
-
-  
   useEffect(() => {
     fetch("/api/time")
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
-        setWidth(700);
-        setHeight(500);
-        setId("root");
+        console.log(data);
       });
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <div>
-            <Link className="App-link" to="/">
-              Home
-            </Link>
-            &nbsp;|&nbsp;
-            <Link className="App-link" to="/page2">
-              Page2
-            </Link>
-          </div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/page2">
-              <p>This is page 2!</p>
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/sample" component={Sample} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/state/atoms.js
@@ -0,0 +1,40 @@
+import { atom } from "recoil";
+
+export const barChartData = atom({
+    key: "barChartData",
+    default: [12, 5, 6, 6, 9, 10],
+});
+
+
+export const regretPlotParam = atom({
+    key: "regretPlotParam",
+    default: {
+        epsilon: 0.05,
+        c: 2,
+        m: 0,
+        nu: 1,
+        alpha: [1, 10],
+        beta: 1,
+        repeats: 100
+    }
+});
+
+
+export const regretPlotData = atom({
+    key: "regretPlotData",
+    default: {
+        times: [],
+        regrets: [],
+        current_time: 0,
+        current_regret: {}
+    }
+});
+
+
+export const allRegretPlotData = atom({
+    key: "allRegretPlotData",
+    default: {
+        times: [],
+        regrets: []
+    }
+});
