48b3d47142cc7ab68cc97490a8be7ff0c7efd364
kenjenlee <kenjen.lee@uwaterloo.ca>
2023-04-01 13:33:45 -0400

    working test version

--- src/components/home/AlgGraph.js
+++ src/components/home/AlgGraph.js
@@ -61,12 +61,11 @@ export default function AlgGraph(props) {
             probabilityHistory: [],
             // },
         },
-        -1
+        -1,
     ]);
 
-    var x_array = [];
+    const [xArray, setXArray] = useState([]);
     var _N = 100;
-    var max_data = 80;
     var margin = 15;
 
     // var unselectIt = function () {
@@ -95,21 +94,24 @@ export default function AlgGraph(props) {
         // });
 
         // for ( var i =0; i < _N; i++ ){
-        // x_array.push( .01*i )
+        // xArray.push( .01*i )
         // }
-        x_array.push(0.0001);
+        xArray.push(0.0001);
         for (var i = 1; i < _N - 1; i++) {
-            x_array.push(0.01 * i);
+            xArray.push(0.01 * i);
         }
-        x_array.push(1 - 0.0001);
+        xArray.push(1 - 0.0001);
+        setXArray(xArray);
 
         theBandits.forEach(function (b, index) {
+            if (index >= numArms) return;
             var theId = "graph_" + index;
 
+            d3.selectAll("#graph_" + index).remove();
             var vis = d3
                 .select("#alggraphparent")
                 .append("svg")
-                .attr("id", "#" + theId)
+                .attr("id", theId)
                 .attr("width", w)
                 .attr("height", h);
 
@@ -128,102 +130,38 @@ export default function AlgGraph(props) {
     }, [theBandits]);
 
     function runBandits() {
-        // console.log("Running bandits");
+        // // console.log("Running bandits");
         var selectedBandit = Math.floor(Math.random() * numArms);
-        // setLastSelectedBanditIndex(selectedBandit);
-        console.log("Bandit index " + selectedBandit + " chosen");
-        // if(selectedBandit === 1) {
-        // console.log("Bandit index " + selectedBandit + " chosen, prior alpha is " +
-        // theBandits[1].alpha  +
-        // " and beta is " +
-        // theBandits[1].beta);
-        // }
-        // var copy = JSON.parse(JSON.stringify(theBandits));
-        var c = cloneDeep(theBandits).__wrapped__;
-        // console.log(c);
-        if (Math.random() < c[selectedBandit].currentActualProbability) {
-            c[selectedBandit].alpha++;
-        } else {
-            c[selectedBandit].beta++;
-        }
-        c[numArms] = selectedBandit;
-        setTheBandits(c);
-        // const updatedBandits = theBandits.map((b, i) => {
-        //     if(i === selectedBandit) {
-        //         var c = cloneDeep(b);
-        //         if (
-        //             Math.random() < b.currentActualProbability
-        //         )
-        //             c.alpha = c.alpha + 1;
-        //         else
-        //             c.beta = c.beta + 1;
-        //         return c;
-        //     } else {
-        //         return b;
-        //     }
-        // })
-        // setTheBandits(updatedBandits);
-
-        // if (
-        //     Math.random() < theBandits[selectedBandit].currentActualProbability
-        // ) {
-        //     setTheBandits(prevBandits => {
-        //         console.log(prevBandits);
-        //         // const allB = [...prevBandits];
-        //         var allB = cloneDeep(prevBandits);
-        //         console.log(prevBandits);
-        //         // // allB[selectedBandit] = [...allB[selectedBandit]];
-        //         // allB[selectedBandit].alpha =
-        //         // allB[selectedBandit].alpha + 1;
-        //         // // if(selectedBandit === 1) {
-        //         //     console.log("Bandit index " + selectedBandit + " chosen, alpha is " +
-        //         //     allB[1].alpha  +
-        //         //     " and beta is " +
-        //         //     allB[1].beta);
-        //         // }
-        //         return allB;
-        //       });
-        //     // success
-        //     // copy[selectedBandit].alpha++;
-        // } else {
-        //     // failure
-        //     setTheBandits(prevBandits => {
-        //         // const allB = [...prevBandits];
-        //         var allB = cloneDeep(prevBandits);
-        //         // allB[selectedBandit] = [...allB[selectedBandit]];
-        //         // allB[selectedBandit].beta =
-        //         // allB[selectedBandit].beta + 1;
-        //         // // if(selectedBandit === 1) {
-        //         //     console.log("Bandit index " + selectedBandit + " chosen, alpha is " +
-        //         //     allB[1].alpha  +
-        //         //     " and beta is " +
-        //         //     allB[1].beta);
-        //         // }
-        //         return allB;
-        //       });
-        //     // copy[selectedBandit].beta++;
-        // }
-        // setTheBandits(copy);
-        // if(selectedBandit === 1) {
-        //     console.log("Bandit index 1 chosen, alpha is " +
-        //     copy[selectedBandit].alpha  +
-        //     " and beta is " +
-        //     copy[selectedBandit].beta);
-        // }
+        const newBanidtsList = theBandits.map((b, i) => {
+            if (i === selectedBandit) {
+                if (Math.random() < b.currentActualProbability) {
+                    b.alpha++;
+                } else {
+                    b.beta++;
+                }
+            } else if (i === numArms) {
+                b = selectedBandit;
+            }
+            return b;
+        });
+        setTheBandits(newBanidtsList);
     }
 
     function update() {
-        console.log("In update");
+        console.log(xArray);
+        // console.log("In update");
         if(theBandits[numArms] < 0) return;
         console.log("Update called for index " + theBandits[numArms]);
         var b = theBandits[theBandits[numArms]];
         var index = theBandits[numArms];
         var alpha = b.alpha;
         var beta = b.beta;
-        var theMax = -1;
+        
         var g = d3.select("#theGraph_" + index);
+        console.log(g);
 
-        var _data = x_array.map(function (x) {
+        var theMax = -1;
+        var _data = xArray.map(function (x) {
             var y = jStat.beta.pdf(x, alpha, beta);
             theMax = Math.max(y, theMax);
             // console.log(y);
@@ -240,35 +178,26 @@ export default function AlgGraph(props) {
                 .domain([0, _N])
                 .range([0 + margin, w - margin]);
 
-        var line = d3
-            .line()
-            .x(function (d, i) {
-                return x(i);
-            })
-            .y((d) => {
-                // console.log(d);
-                return d;
-            });
-        d3.selectAll("#line-" + index).remove();
-        g.selectAll("path.line")
-            .data([_data])
-            .enter()
-            .append("path")
-            // .join("path")
+        var d = [];
+        var xt10 = x.ticks(_N);
+        for (var i = 0; i < _N; i++) {
+            d.push([x(xt10[i]), y(_data[i])]);
+        }
+        console.log(alpha, beta, xArray, _data, d);
+        var line = d3.line();
+        
+        d3.selectAll("#line_" + index).remove();
+        g.append("path")
+            .attr("d", line(d))
             .attr("stroke", "#0000FF")
+            .attr("id", "line_" + index)
             .attr("fill", "none")
-            .attr("class", "area")
-            .attr("d", line)
-            .attr("id", "line-" + index)
-            .attr("stroke-width", "2px")
-            .attr(
-                "titleIgnore",
-                "This is the current distribution for the probability of success (it is a beta distribution with parameters alpha=1+#successes, beta=1+#misses).  Each time the bandit is tried, this distribution will get updated based on whether there was a success or not."
-            );
+            .attr("stroke-width", "2px");
     }
 
     function draw() {
         theBandits.forEach(function (b, index) {
+            if (index == numArms) return;
             var theId = "graph_" + index;
             // var w = that.defaultWidth; //400;
             // var h = that.defaultHeight; //100;
@@ -287,8 +216,9 @@ export default function AlgGraph(props) {
 
             var theMax = -1;
 
-            var _data = x_array.map(function (x) {
+            var _data = xArray.map(function (x) {
                 var y = jStat.beta.pdf(x, alpha, beta);
+                // var y = jStat.beta.pdf(x, 1, 2);
                 theMax = Math.max(y, theMax);
                 // console.log(y);
                 return y;
@@ -296,6 +226,7 @@ export default function AlgGraph(props) {
                 //            return jStat.beta.pdf(x,alpha,beta);
             });
             // console.log(_data);
+            // console.log(theMax);
 
             var y = d3
                     .scaleLinear()
@@ -306,110 +237,38 @@ export default function AlgGraph(props) {
                     .domain([0, _N])
                     .range([0 + margin, w - margin]);
 
-            // (y = d3.scaleLinear()
-            //     .domain([0, max_data])
-            //     .range([h - margin, 0 + margin])),
-            //     (x = d3.scaleLinear()
-            //         .domain([0, _N])
-            //         .range([0 + margin, w - margin]));
+            var d = [];
+            var xt10 = x.ticks(_N);
+            for (var i = 0; i < _N; i++) {
+                d.push([x(xt10[i]), y(_data[i])]);
+            }
+            var line = d3.line();
 
-            var line = d3
-                .line()
-                .x(function (d, i) {
-                    return x(i);
-                })
-                .y((d) => {
-                    // console.log(d);
-                    return d;
-                });
-            // .y(y);
-
-            // console.log(line);
-
-            //console.log(_data);
-            g.selectAll("path.line")
-                .data([_data])
-                .enter()
-                .append("path")
+            g.append("path")
+                .attr("d", line(d))
                 .attr("stroke", "#0000FF")
+                .attr("id", "line_" + index)
                 .attr("fill", "none")
-                .attr("class", "area")
-                .attr("d", line)
-                .attr("id", "line-" + index)
-                .attr("stroke-width", "2px")
-                .attr(
-                    "titleIgnore",
-                    "This is the current distribution for the probability of success (it is a beta distribution with parameters alpha=1+#successes, beta=1+#misses).  Each time the bandit is tried, this distribution will get updated based on whether there was a success or not."
-                );
+                .attr("stroke-width", "2px");
 
             g.append("svg:line")
                 .attr("x1", x(0))
                 .attr("y1", y(0))
-                .attr("x2", x(w))
+                // .attr("x2", x(w))
+                .attr("x2", x(_N))
                 .attr("y2", y(0))
-                .attr("stroke", "#000000");
+                .attr("stroke", "#000000")
+                .attr("id", "xAxis_" + index);
 
             g.append("svg:line")
                 .attr("x1", x(0))
                 .attr("y1", y(0))
                 .attr("x2", x(0))
-                .attr("y2", y(max_data))
-                .attr("stroke", "#000000");
+                // .attr("y2", y(max_data))
+                .attr("y2", y(theMax))
+                .attr("stroke", "#000000")
+                .attr("id", "yAxis_" + index);
 
-            //     //console.log(_N*b.currentActualProbability);
-            // g.append("svg:line")
-            //     .attr("id", "actualProb_" + index)
-            //     .attr(
-            //         "x1",
-            //         margin + (w - 2 * margin) * b.currentActualProbability
-            //     )
-            //     .attr("y1", y(0))
-            //     .attr(
-            //         "x2",
-            //         margin + (w - 2 * margin) * b.currentActualProbability
-            //     )
-            //     .attr("y2", y(max_data))
-            //     .attr("stroke", "#000000")
-            //     .attr("stroke-width", "2px")
-            //     .attr("banditIndex", index)
-            //     .attr("class", "actualProbabilityLine")
-            //     .attr(
-            //         "titleIgnore",
-            //         "This is the current actual probability of success for this bandit.  If it is lower than that of the other bandits, then the bandit may be tried rarely; if it changes later to a high value, it may take a while for the bandit to be tried again so that the algorithm can start learning that it has become better."
-            //     );
-            // .on("mouseover", function () {
-            //     d3.select(this).classed("lineActive", true);
-            // })
-            // .on("mousedown", function () {
-            //     //d3.select(this).classed("lineSelected",true);
-            //     that.currentLineSelected = d3.select(this);
-            //     //var theThing = d3.select(this);
-            //     //console.log(d3.select(this));
-            // })
-            // .on("mouseout", function () {
-            //     if (!that.currentLineSelected) {
-            //         d3.select(this).classed("lineActive", false);
-            //     }
-            // });
-
-            //     // .on("mousemove",function() {
-            //     // //d3.select(this).classed("lineSelected",false);
-            //     // })
-
-            //     // var drag = d3.behavior.drag()
-            //     // .on("drag", function(d,i) {
-            //     // d.x += d3.event.dx;
-            //     // d.y += d3.event.dy;
-            //     // var thing = d3.select(this);
-            //     // if (thing.classed("lineSelected")) {
-            //     //
-            //     // console.log(thing);
-            //     //
-            //     // thing.attr("transform", function(d,i){
-            //     // return "translate(" + [ d.x,d.y ] + ")"
-            //     // })
-            //     // }
-            //     // });
             g.selectAll(".xLabel")
                 // fix floating point precision issues
                 .data(d3.range(0, 1.2, 0.1).map((e) => e.toFixed(1)))
@@ -458,31 +317,13 @@ export default function AlgGraph(props) {
                     "x2",
                     margin + (w - 2 * margin) * b.currentActualProbability
                 )
-                .attr("y2", y(max_data))
+                // .attr("y2", y(max_data))
+                .attr("y2", y(theMax))
                 .attr("stroke", "#000000")
                 .attr("stroke-width", "2px")
                 .attr("banditIndex", index)
                 .attr("class", "actualProbabilityLine");
-            // .attr(
-            //     "titleIgnore",
-            //     "This is the current actual probability of success for this bandit.  If it is lower than that of the other bandits, then the bandit may be tried rarely; if it changes later to a high value, it may take a while for the bandit to be tried again so that the algorithm can start learning that it has become better."
-            // )
-            // .on("mouseover", function () {
-            //     d3.select(this).classed("lineActive", true);
-            // })
-            // .on("mousedown", function () {
-            //     //d3.select(this).classed("lineSelected",true);
-            //     that.currentLineSelected = d3.select(this);
-            //     //var theThing = d3.select(this);
-            //     //console.log(d3.select(this));
-            // })
-            // .on("mouseout", function () {
-            //     if (!that.currentLineSelected) {
-            //         d3.select(this).classed("lineActive", false);
-            //     }
-            // });
-
-            //     that.theVis[index] = vis;
+            console.log(xArray);
         });
     }
 
