241dfaf888597d3c68290509d0865e8c527a39f5
kenjenlee <kenjen.lee@uwaterloo.ca>
2023-04-04 13:54:14 -0400

    post.js reacts to play pause button in timeController

--- src/components/socialMediaApp/post.js
+++ src/components/socialMediaApp/post.js
@@ -16,155 +16,173 @@ import { allSettingsParam } from "../../state/atoms";
 import { interval } from "d3";
 
 export const Post = forwardRef((props, ref) => {
+  const [mode, setMode] = useState("manual");
+  const [play, setPlay] = useState(false);
+  const [likeButtonColor, setLikeButtonColor] = useState({
+    color: "#C6C6C6",
+  });
+  const [numOfLikes, setNumOfLikes] = useState(props.postInfo.numOfLikes);
+  const [liked, setLiked] = useState(false);
+  const [swipingUp, setSwipingUp] = useState(false);
+  const [timeoutIDAutoLike, setTimeoutIDAutoLike] = useState(null);
+  const [timeoutIDAutoSwipe, setTimeoutIDAutoSwipe] = useState(null);
 
-    const [mode, setMode] = useState("manual");
-    const [likeButtonColor, setLikeButtonColor] = useState({
-        color: "#C6C6C6",
-    });
-    const [numOfLikes, setNumOfLikes] = useState(props.postInfo.numOfLikes);
-    const [liked, setLiked] = useState(false);
-    const [swipingUp, setSwipingUp] = useState(false);
+  const allSettingsParamValue = useRecoilValue(allSettingsParam);
 
-    const allSettingsParamValue = useRecoilValue(allSettingsParam);
-
-    useEffect(() => {
-        // to manual mode
-        if (mode != "manual" && allSettingsParamValue.currentMode == "manual") {
-            setMode("manual")            
-        }
-        // to automatic mode
-        else if (mode != "automatic" && allSettingsParamValue.currentMode == "automatic") {
-            setMode("automatic")
-            runingAutoMode();
-        }
-      }, [allSettingsParamValue]);
-
-    // run auto mode and automatically run functions every 1 and 3 seconds
-    const runingAutoMode = () => {
-        setTimeout(autoClickLike, 1000);      
-        setTimeout(autoSwipeUp, 4000);
+  useEffect(() => {
+    if (
+      allSettingsParamValue.currentMode == "automatic" &&
+      allSettingsParamValue.play
+    ) {
+      runingAutoMode();
     }
+    // console.log(
+    //   "in use effect, gonna set mode to " +
+    //     allSettingsParamValue.currentMode +
+    //     " and play is " +
+    //     allSettingsParamValue.play
+    // );
+    setMode(allSettingsParamValue.currentMode);
+    setPlay(allSettingsParamValue.play);
+  }, [allSettingsParamValue]);
 
-    const autoClickLike = () => {
-        if (props.autoClickLike) {
-            setLikeButtonColor({ color: "#E91E63" });
-            setNumOfLikes(props.postInfo.numOfLikes + 1);
-            setLiked(!liked);
-        }
+  useEffect(() => {
+    if (!play) {
+      if (timeoutIDAutoLike !== undefined) {
+        clearTimeout(timeoutIDAutoLike);
+      }
+      if (timeoutIDAutoSwipe !== undefined) {
+        clearTimeout(timeoutIDAutoSwipe);
+      }
     }
+  }, [play, timeoutIDAutoLike, timeoutIDAutoSwipe]);
 
-    const autoSwipeUp = () => {
-        if (props.postInfo.userName == "mAtrIx") {
-            props.onUserAction(-100.0);
+  // run auto mode and automatically run functions every 1 and 3 seconds
+  const runingAutoMode = () => {
+    setTimeoutIDAutoLike(setTimeout(autoClickLike, 1000));
+    setTimeoutIDAutoSwipe(setTimeout(autoSwipeUp, 4000));
+  };
+
+  const autoClickLike = () => {
+    if (props.autoClickLike && mode != "manual") {
+      setLikeButtonColor({ color: "#E91E63" });
+      setNumOfLikes(props.postInfo.numOfLikes + 1);
+      setLiked(!liked);
+    }
+  };
+
+  const autoSwipeUp = () => {
+    console.log("IN autoSwipeUp, play is " + play);
+    // if (play) {
+    if (props.postInfo.userName == "mAtrIx") {
+      props.onUserAction(-100.0);
+    } else {
+      if (liked) {
+        props.onUserAction(1.0);
+      } else {
+        props.onUserAction(0.0);
+      }
+    }
+    // }
+  };
+
+  // reset the page
+  const resetPost = () => {
+    setLikeButtonColor({ color: "#C6C6C6" });
+    setNumOfLikes(props.postInfo.numOfLikes);
+    setLiked(false);
+    // console.log("done resetting");
+  };
+
+  // using the module for detecting swiping
+  // from https://www.npmjs.com/package/react-swipe-component
+  const onSwipeEnd = () => {
+    if (swipingUp && mode == "manual") {
+      // console.log("Swiped Up");
+
+      if (props.postInfo.userName == "mAtrIx") {
+        props.onUserAction(-100.0);
+      } else {
+        if (liked) {
+          props.onUserAction(1.0);
         } else {
-            if (liked) {
-                props.onUserAction(1.0);
-            } else {
-                props.onUserAction(0.0);
-            }
+          props.onUserAction(0.0);
         }
+      }
+      setSwipingUp(false);
     }
+  };
 
-    // reset the page
-    const resetPost = () => {
+  const onSwipeUpListener = () => {
+    setSwipingUp(true);
+  };
+
+  // like button handler in mannual mode
+  const handleLikeButton = (e) => {
+    if (mode == "manual") {
+      // dislike the post
+      if (liked) {
         setLikeButtonColor({ color: "#C6C6C6" });
         setNumOfLikes(props.postInfo.numOfLikes);
-        setLiked(false);
-        // console.log("done resetting");
-    };
+        setLiked(!liked);
+      }
+      // like the post
+      else {
+        setLikeButtonColor({ color: "#E91E63" });
+        setNumOfLikes(props.postInfo.numOfLikes + 1);
+        setLiked(!liked);
+      }
+    }
+  };
 
-    // using the module for detecting swiping
-    // from https://www.npmjs.com/package/react-swipe-component
-    const onSwipeEnd = () => {
-        if (swipingUp && mode == "manual") {
-            // console.log("Swiped Up");
+  // preventing dragging images
+  // from https://stackoverflow.com/questions/49898749/how-can-i-prevent-drag-and-drop-images-in-a-react-app
+  const preventDragHandler = (e) => {
+    e.preventDefault();
+  };
 
-            if (props.postInfo.userName == "mAtrIx") {
-                props.onUserAction(-100.0);
-            } else {
-                if (liked) {
-                    props.onUserAction(1.0);
-                } else {
-                    props.onUserAction(0.0);
-                }
-            }
-            setSwipingUp(false);
-        }
-    };
-
-    const onSwipeUpListener = () => {
-        setSwipingUp(true);
-    };
-
-    // like button handler in mannual mode
-    const handleLikeButton = (e) => {
-        if (mode == "manual") {
-            // dislike the post
-            if (liked) {
-                setLikeButtonColor({ color: "#C6C6C6" });
-                setNumOfLikes(props.postInfo.numOfLikes);
-                setLiked(!liked);
-            }
-            // like the post
-            else {
-                setLikeButtonColor({ color: "#E91E63" });
-                setNumOfLikes(props.postInfo.numOfLikes + 1);
-                setLiked(!liked);
-            }
-        }
-    };
-
-    // preventing dragging images
-    // from https://stackoverflow.com/questions/49898749/how-can-i-prevent-drag-and-drop-images-in-a-react-app
-    const preventDragHandler = (e) => {
-        e.preventDefault();
-    };
-
-    return (
-        <Card style={{ width: "90%" }} id="social-media-app-card">
-            <Swipe
-                nodeName="div"
-                onSwipeEnd={onSwipeEnd}
-                onSwipedUp={onSwipeUpListener}
-            >
-                <CardHeader
-                    className="userProfile"
-                    sx={{ "user-select": "none", textAlign: "left" }}
-                    avatar={
-                        <Avatar
-                            sx={props.postInfo.avatarColor}
-                            aria-label="user avatar"
-                        >
-                            {props.postInfo.avatar}
-                        </Avatar>
-                    }
-                    title={props.postInfo.userName}
-                    subheader={props.postInfo.date}
-                />
-                <CardMedia
-                    component="img"
-                    image={props.postInfo.imgSrc}
-                    sx={{ height: "300px", width: "300px" }}
-                    onDragStart={preventDragHandler}
-                    onDoubleClick={handleLikeButton}
-                />
-                <CardActions disableSpacing className="userAction">
-                    <IconButton
-                        size="large"
-                        aria-label="like this post"
-                        onClick={handleLikeButton}
-                    >
-                        <FavoriteIcon sx={likeButtonColor} />
-                    </IconButton>
-                    <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        sx={{ "user-select": "none" }}
-                    >
-                        {numOfLikes} likes
-                    </Typography>
-                </CardActions>
-            </Swipe>
-        </Card>
-    );
+  return (
+    <Card style={{ width: "90%" }} id="social-media-app-card">
+      <Swipe
+        nodeName="div"
+        onSwipeEnd={onSwipeEnd}
+        onSwipedUp={onSwipeUpListener}
+      >
+        <CardHeader
+          className="userProfile"
+          sx={{ "user-select": "none", textAlign: "left" }}
+          avatar={
+            <Avatar sx={props.postInfo.avatarColor} aria-label="user avatar">
+              {props.postInfo.avatar}
+            </Avatar>
+          }
+          title={props.postInfo.userName}
+          subheader={props.postInfo.date}
+        />
+        <CardMedia
+          component="img"
+          image={props.postInfo.imgSrc}
+          sx={{ height: "300px", width: "300px" }}
+          onDragStart={preventDragHandler}
+          onDoubleClick={handleLikeButton}
+        />
+        <CardActions disableSpacing className="userAction">
+          <IconButton
+            size="large"
+            aria-label="like this post"
+            onClick={handleLikeButton}
+          >
+            <FavoriteIcon sx={likeButtonColor} />
+          </IconButton>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ "user-select": "none" }}
+          >
+            {numOfLikes} likes
+          </Typography>
+        </CardActions>
+      </Swipe>
+    </Card>
+  );
 });
